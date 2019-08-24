7c5933604f711e9c92c1e02fe30062b4c2e62f37
areum15 <thd7055@gmail.com>
2019-08-24 16:48:07 +0900

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// 펑션 컴포넌트는 반드시 대문자로 시작
+// react element를 리턴해야 한다.
+export const Header = ({title, totalPlayers}) => {
+    return (
+        <header className="header">
+            <h1 className="h1">{title}</h1>
+            <span className="stats">Players: {totalPlayers}</span>
+        </header>
+    )
+}
