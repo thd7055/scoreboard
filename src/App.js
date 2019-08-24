c410e22d0adb7b38beb4fe8fb6e732e19fc9e90c
areum15 <thd7055@gmail.com>
2019-08-24 16:56:54 +0900

    리팩토링 - header, player, count

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import { Player } from "./components/Player";
+
 
 // // 펑션 컴포넌트는 반드시 대문자로 시작
 // // react element를 리턴해야 한다.
@@ -15,44 +17,44 @@ import {Header} from "./components/Header";
 
 
 
-class Counter extends React.Component {
-  state = {
-    score: 0,
-    a: 10
-  }
-  constructor() {
-    super();
-    this.handleChangeScore = this.handleChangeScore.bind(this);
-  }
+// class Counter extends React.Component {
+//   state = {
+//     score: 0,
+//     a: 10
+//   }
+//   constructor() {
+//     super();
+//     this.handleChangeScore = this.handleChangeScore.bind(this);
+//   }
+//
+//   handleChangeScore(delta) {
+//     console.log('incrementScore', this);
+//     // state를 변경하는 방법은 setState() 밖에 없다.
+//     // this.state.score += 1;
+//     this.setState(prevState => ({score: prevState.score + delta}));
+//   }
+//
+//   // 이벤트의 오른쪽은 함수 선언문이 들어가야 한다.
+//   render() {
+//     return (
+//       <div className="counter">
+//         <button className="counter-action decrement" onClick={() => this.handleChangeScore(-1)}> - </button>
+//         <span className="counter-score">{this.state.score}</span>
+//         <button className="counter-action increment" onClick={() => this.handleChangeScore(1)}> + </button>
+//       </div>
+//     )
+//   }
+// }
 
-  handleChangeScore(delta) {
-    console.log('incrementScore', this);
-    // state를 변경하는 방법은 setState() 밖에 없다.
-    // this.state.score += 1;
-    this.setState(prevState => ({score: prevState.score + delta}));
-  }
-
-  // 이벤트의 오른쪽은 함수 선언문이 들어가야 한다.
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.handleChangeScore(-1)}> - </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={() => this.handleChangeScore(1)}> + </button>
-      </div>
-    )
-  }
-}
-
-const Player = (props) => (
-  <div className="player">
-		<span className="player-name">
-			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
-      {props.name}
-		</span>
-    <Counter/>
-  </div>
-)
+// const Player = (props) => (
+//   <div className="player">
+// 		<span className="player-name">
+// 			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
+//       {props.name}
+// 		</span>
+//     <Counter/>
+//   </div>
+// )
 
 class App extends React.Component {
   state = {
