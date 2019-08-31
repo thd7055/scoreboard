7cb4d8d7cfd17e833665f9626e7323a8a86dfb77
Areum <thd7055@naver.com>
2019-08-31 12:58:42 +0900

    deep copy

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,26 +3,34 @@ import React from 'react'
 export class Stopwatch extends React.Component {
     tickRef;
     state = {
-        isRunning: false
+        isRunning: false,
+        timer: 0
     }
 
     handleStopwathch = () =>{
-        this.setState(prevState => {isRunning: !prevState.isRunning})
+        this.setState(prevState => ({isRunning: !prevState.isRunning}))
+    }
+
+    handleReset = () => {
+        this.setState(prevState => ({timer: 0}))
     }
     render() {
         return (
             <div className="stopwatch">
                 <h2>stopWatch</h2>
-                <span className="stopwatch-time">5</span>
+                <span className="stopwatch-time">{this.state.timer}</span>
                 {/*시간에 따라 변하는 데이터 state로 관리해야한다.*/}
-                <button>{this.state.isRunning ? 'Stop' : 'Start'}</button>
-                <button>Reset</button>
+                <button onClick={this.handleStopwathch}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
+                <button onClick={this.handleReset}>Reset</button>
             </div>
         );
     }
 
     tick = () => {
         // time update logic
+        if(this.state.isRunning){
+            this.setState(prevState => ({timer: prevState.timer +1}));
+        }
     }
 
     //DOM이 렌더링 된 직후에 호출되는 라이프 사이클
