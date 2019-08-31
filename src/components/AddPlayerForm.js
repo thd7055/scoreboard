75b955ba5fc38c4fdf550b0e3e8ceef2731806e7
Areum <thd7055@naver.com>
2019-08-31 14:49:59 +0900

    uncontrolled component: ref로 dom 얻기

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export class AddPlayerForm extends React.Component {
-    state = {
+    /*state = {
         value : ''
     }
 
     handleValueChange = (e) => {
         this.setState({value: e.target.value});
-    }
+    }*/
 
+    textInput = React.createRef()
     handleSubmit = (e) => {
         // submit의 기본 이벤트 막기(깜박거림)
         e.preventDefault();
@@ -20,7 +21,9 @@ export class AddPlayerForm extends React.Component {
         console.log(form.checkValidity());
 
         // 3. 부모에게서 받은 콜백 function을 호출
-        this.props.addPlayer(this.state.value);
+        // this.props.addPlayer(this.state.value);
+        // this.textInput.current. === document.getElementById("player");
+        this.props.addPlayer(this.textInput.current.value);
     }
     render()
     {
@@ -28,7 +31,8 @@ export class AddPlayerForm extends React.Component {
             /* noValidate : html5 속성을 체크하지 않는다 */
             <form id="form" className="form" onSubmit={this.handleSubmit} noValidate>
                 {/* required : 필수값을 입력하지 않으면 validation 체크*/}
-                <input id="player" className="input" type="text" placeholder="enter a player's name" value={this.state.value} onChange={this.handleValueChange} required></input>
+                <input id="player" className="input" type="text" placeholder="enter a player's name"
+                       ref={this.textInput} /*value={this.state.value} onChange={this.handleValueChange}*/ required></input>
                 <input className="input" type="submit" value="Add Player"></input>
             </form>
         );
